752789afd582658efaeab04feb85d87da9e1f188
JYC0413 <juyichen0413@foxmail.com>
2024-02-23 16:56:51 +0800

    update

---
+++ change/index.d.ts
@@ -0,0 +1,6 @@
+export { default as ChatItem, type ChatItemProps } from './ChatItem';
+export { default as EditableMessage, type EditableMessageProps } from './EditableMessage';
+export { Input, type InputProps, TextArea, type TextAreaProps } from './Input';
+export { default as Markdown, type MarkdownProps } from './Markdown';
+export { default as MobileNavBar, type MobileNavBarProps } from './MobileNavBar';
+export { default as MobileNavBarTitle, type MobileNavBarTitleProps, } from './MobileNavBar/MobileNavBarTitle';
---
+++ change/index.js
@@ -0,0 +1,5 @@
+export { default as ChatItem } from "./ChatItem";
+export { default as EditableMessage } from "./EditableMessage";
+export { Input, TextArea } from "./Input";
+export { default as MobileNavBar } from "./MobileNavBar";
+export { default as MobileNavBarTitle } from "./MobileNavBar/MobileNavBarTitle";
